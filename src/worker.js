800a02c6eed99d17e5dd003ff58ec77765668568
Mahfuz Alam <mahfuz.alam.shohan@gmail.com>
2025-12-17 10:25:59 +0600

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,7 +1,7 @@
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 import { sign, verify } from 'hono/jwt';
-import { serveStatic } from 'hono/cloudflare-workers';
+// Removed unused import: import { serveStatic } from 'hono/cloudflare-workers';
 import { serveAdminHtml } from './templates/adminApp.js';
 import { serveSharedHtml } from './templates/sharedView.js';
 
